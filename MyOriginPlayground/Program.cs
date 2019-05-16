3f16f092113811974259cd8f6dd3be2626c1f1fc
unknown <49011@ksc.local>
2019-05-16 09:57:53 +0900

    プロジェクト ファイルを追加します。

---
+++ MyOriginPlayground/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOriginPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = int.Parse(Console.ReadLine()); // 標準入力
+            string[] str = Console.ReadLine().Split(' ');
+            b = int.Parse(str[0]);
+            c = int.Parse(str[1]);
+        }
+    }
+}
